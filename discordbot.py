a7083d1cade8aac052d94ed5de488dabced8fe0e
JangMinseok1211 <74437788+JangMinseok1211@users.noreply.github.com>
2023-03-25 13:09:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -15,7 +16,7 @@ async def on_ready():
     print(f'Logged in as {client.user}.')
 
 @client.event
-      
+      import asyncio
 if "집합" in message.content:
     await message.delete()
     await message.channel.send(f"{message.author.mention} 님이 집합 시킵니다. @everyone")
