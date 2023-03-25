e55ea27248595fe98e549a32e08776589d91b7d2
JangMinseok1211 <74437788+JangMinseok1211@users.noreply.github.com>
2023-03-25 13:54:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,6 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -16,15 +15,14 @@ async def on_ready():
     print(f'Logged in as {client.user}.')
 
 @client.event
-
-async def hello(ctx):
-  await ctx.reply('Hi, there!')
-
-#async def user_message(message):
- #   if message.author == client.user:
-  #      return
-   # if message.content.startswith(f'집합'):
-    #    await message.channel.send('{message.author.mention} 님이 집합 시킵니다. @everyone')
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == f'call':
+        await mesaage.channel.send("callback!")
+        
+    if message.content.startswith(f'집합'):
+        await message.channel.send('집합. @everyone')
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
